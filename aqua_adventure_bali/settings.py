54ad5212cdbe073feef486d48163f7e8dade8114
Paima Ishak Melkisedek Purba <paima@Paimas-MacBook-Pro.local>
2024-10-09 16:50:16 +0700

    add

--- aqua_adventure_bali/settings.py
+++ aqua_adventure_bali/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-f74nvf9g1zkhk3_@3iimgkydt=*gjjpi5^#07d6o%)u4zdf!%-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://paima-ishak-aquaadventurebali.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "paima-ishak-aquaadventurebali.pbp.cs.ui.ac.id"]
 
 
 # Application definition
