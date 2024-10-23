21f5d7836446b51758e53a59a084bd2d88ed7074
reyvanomario <reyvanomario24122004@gmail.com>
2024-10-23 16:37:01 +0700

    buat fungsi login

--- main/views.py
+++ main/views.py
@@ -1,11 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import render, reverse
+from django.http import HttpResponse
+from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
     return render(request, "main.html", {})
 
 def login(request):
-    return render(request, "login.html", {})
+  if request.method == 'POST':
+    form = AuthenticationForm(data=request.POST)
+
+    if form.is_valid():
+      user = form.get_user()
+      login(request, user)
+      response = HttpResponseRedirect(reverse("main:show_main"))
+      response.set_cookie('last_login', str(datetime.datetime.now()))
+      return response
+    
+    else:
+      messages.error(request, "Invalid username or password. Please try again.")
+
+    
+  else:
+    form = AuthenticationForm(request)
+  context = {'form': form}
+  return render(request, 'login.html', context)
 
 def register(request):
     return render(request, "register.html", {})
