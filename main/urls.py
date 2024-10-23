8ee096b93021c10ffe601f467dedc997af28965d
William Alexander <williamalexander@Williams-MacBook-Air.local>
2024-10-23 15:46:44 +0700

    template login & register created

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, login, register
 
 urlpatterns = [
-    path("", show_main, name="show_main")
+    path("", show_main, name="show_main"),
+    path("login/", login, name="login"),
+    path("register/", register, name="register"),
 ]
