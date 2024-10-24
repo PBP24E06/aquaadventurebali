81c040aa25ab929a802eb1d2dcdd0af1a2ee6ebd
reyvanomario <reyvanomario24122004@gmail.com>
2024-10-24 16:42:56 +0700

    membuat model CartItem

--- main/models.py
+++ main/models.py
@@ -49,8 +49,18 @@ class Wishlist(models.Model):
     
 
 class Cart(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name="cart", on_delete=models.CASCADE)  # Relasi balik ke user
+
+    def total_price(self):
+        return sum(item.total_price() for item in self.items.all())
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='cart_items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def total_price(self):
+        return self.product.harga * self.quantity
 
 
 class Report(models.Model):
