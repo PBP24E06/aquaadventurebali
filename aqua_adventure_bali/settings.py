29cd7f97c4b3a17bb378654574713eb48fedfffe
Paima <paimapurba@gmail.com>
2024-10-27 17:21:19 +0700

    test

--- aqua_adventure_bali/settings.py
+++ aqua_adventure_bali/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-f74nvf9g1zkhk3_@3iimgkydt=*gjjpi5^#07d6o%)u4zdf!%-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 PRODUCTION = os.getenv("PRODUCTION", False)
-DEBUG = True
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "paima-ishak-aquaadventurebali.pbp.cs.ui.ac.id"]
 
